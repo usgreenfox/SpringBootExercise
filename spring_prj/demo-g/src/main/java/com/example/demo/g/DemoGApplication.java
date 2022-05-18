6c2fe64edd826fd7d8380f68d2876a89beb4053f
usgreenfox <usgreenfox@gmail.com>
2022-05-18 20:20:01 +0900

    create develop env

---
+++ spring_prj/demo-g/src/main/java/com/example/demo/g/DemoGApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGApplication.class, args);
+	}
+
+}
