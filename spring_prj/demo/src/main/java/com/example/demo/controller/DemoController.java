a5d9595a38cf96a4796b352915125c3f92307d34
usgreenfox <usgreenfox@gmail.com>
2022-07-18 09:30:30 +0900

    learn java array comparison

--- spring_prj/demo/src/main/java/com/example/demo/controller/DemoController.java
+++ spring_prj/demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,13 +1,22 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.DemoService;
 
 @Controller
 public class DemoController {
 
+  @Autowired
+  DemoService demoServ;
+
   @RequestMapping("/")
   public String home() {
+    // demoServ.streamOptionalDemo();
+    demoServ.arrayComparison();
+
     return "home";
   }
 }
--- spring_prj/demo/src/main/java/com/example/demo/service/DemoService.java
+++ spring_prj/demo/src/main/java/com/example/demo/service/DemoService.java
@@ -1,48 +1,76 @@
 package com.example.demo.service;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-
-import com.example.demo.dto.DeviceAttribute;
-import com.example.demo.dto.DeviceInformation;
-import com.example.demo.dto.DeviceStructureDto;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class DemoService {
-  
+  /**
+   * StreamとOptionalの練習
+   * findFirst(),findAny()
+   * 
+   */
+  public void streamOptionalDemo() {
 
-  public DeviceStructureDto demo() {
-    DeviceStructureDto dto = new DeviceStructureDto();
-    DeviceInformation device = new DeviceInformation();
-    DeviceAttribute attribute1 = new DeviceAttribute();
-    attribute1.setProperty("name");
-    attribute1.setValue("Foo");
-    DeviceAttribute attribute2 = new DeviceAttribute();
-    attribute2.setProperty("height");
-    attribute2.setValue("170");
-    device.setAttributes(Arrays.asList(attribute1, attribute2));
-    dto.setDevices(Arrays.asList(device));
-
-    dto.getDevices().forEach( d -> {
-      d.getAttributes().forEach(attr -> {
-        propertyToField(dto, attr.getProperty(), attr.getValue());
-      });
-    });
-    return null;
   }
 
+  /**
+   * 配列における比較方法の確認
+   * ==, equals, deepEquals
+   * 
+   */
+  public void arrayComparison() {
+    
+    String[] array1 = {"aaa", "bbb", "ccc" };
 
+    // 元の配列を代入　→ 値も参照先も同じ    
+    String[] asArray1 = array1;
+    // 元の配列をコピー　→ 値は一緒だが参照先は異なる
+    String[] copyArray1 = Arrays.copyOf(array1, 3);
+    // 元の配列を同じ値を代入　→　値は一緒だが参照先は異なる
+    String[] likeArray1 = {"aaa", "bbb", "ccc" };
 
-  public void propertyToField(DeviceStructureDto dto, String propertyId, String value) {
-    switch(propertyId) {
-      case "NO":
-        dto.setNo(value);
-        break;
-      default:
-        break;
-    }
+    // ==
+    System.out.println(array1 == asArray1); /* true */
+    System.out.println(array1 == copyArray1); /* false */
+    System.out.println(array1 == likeArray1); /* false */
+
+    // equals
+    System.out.println(Arrays.equals(array1, asArray1)); /* true */
+    System.out.println(Arrays.equals(array1, copyArray1)); /* true */
+    System.out.println(Arrays.equals(array1, likeArray1)); /* true */
+
+    // deepEquals
+    System.out.println(Arrays.deepEquals(array1, asArray1)); /* true */
+    System.out.println(Arrays.deepEquals(array1, copyArray1)); /* true */
+    System.out.println(Arrays.deepEquals(array1, likeArray1)); /* true */
+
+    // 二次元配列の場合
+    String[][] array2 = {{"aaa", "bbb", "ccc" }, {"ddd", "eee", "fff"}};
+
+    String[][] asArray2 = array2;
+    String[][] copyArray2 = Arrays.copyOf(array2, 2);
+    String[][] likeArray2 = {{"aaa", "bbb", "ccc" }, {"ddd", "eee", "fff"}};
+
+    // ==
+    System.out.println(array2 == asArray2); /* true */
+    System.out.println(array2 == copyArray2); /* false */
+    System.out.println(array2 == likeArray2); /* false */
+
+    // equals
+    // equalsは配列の中の参照先が同じか判断している。
+    // likeArray2は"配列の中の配列"の参照先が異なるため、falseとなる
+    System.out.println(Arrays.equals(array2, asArray2)); /* true */
+    System.out.println(Arrays.equals(array2, copyArray2)); /* true */
+    System.out.println(Arrays.equals(array2, likeArray2)); /* false */
+
+    // deepEquals
+    // "配列の中の配列"の値が同じか判断するため、全てtrueとなる。
+    System.out.println(Arrays.deepEquals(array2, asArray2));  /* true */
+    System.out.println(Arrays.deepEquals(array2, copyArray2));  /* true */
+    System.out.println(Arrays.deepEquals(array2, likeArray2));  /* true */
+
   }
+
 }
